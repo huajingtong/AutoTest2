8cc4e436a96570443144c62bc7f4ddbed4be4b44
huajingtong <934395737@qq.com>
2018-06-24 19:33:13 +0800

    Initial commit

---
+++ src/main/java/com/huajingtong/test1/autotest1.java
@@ -0,0 +1,10 @@
+package com.huajingtong.test1;
+
+/**
+ * Created by hjt on 2018/6/24.
+ */
+public class autotest1 {
+    public static void main(String[] args) {
+        System.out.println("为啥我学东西这么慢呢");
+    }
+}
