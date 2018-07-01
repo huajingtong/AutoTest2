c43149adc046fee1702cbd6ff7ca4f323ac39a65
huajingtong <934395737@qq.com>
2018-07-01 13:35:20 +0800

    嘻嘻aa111

--- src/main/java/com/huajingtong/test1/paul.java
+++ src/main/java/com/huajingtong/test1/paul.java
@@ -4,5 +4,7 @@ package com.huajingtong.test1;
  * Created by hjt on 2018/7/1.
  */
 public class paul {
-
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaa");
+    }
 }
